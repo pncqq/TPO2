c14d0c0f63f998f2c09339dd530ed240dabae5c7
Filip Michalski <s24916@pjwstk.edu.pl>
2023-04-04 12:03:34 +0100

    Dodano biblioteke Nv_i18n Zakomentowano GUI Dodano czytanie pogody

--- src/Service.java
+++ src/Service.java
@@ -3,39 +3,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+
+import com.neovisionaries.i18n.CountryCode;
 import org.json.JSONObject;
+
+import static jdk.nashorn.internal.objects.NativeMath.round;
 
 
 public class Service {
+    private static final String API_KEY = "e2cdb03aec582b3e57b0a56cdb06df3a";
+    private String kraj;
+
     public Service(String kraj) {
+        this.kraj = kraj;
     }
 
     // Metoda pobierająca informacje o pogodzie z serwisu OpenWeatherMap
-    private static String getWeather(String city) {
-        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=API_KEY&units=metric";
-        String weather = "";
+    public String getWeather(String miasto) {
+        //Otrzymanie kodu kraju
+        List<CountryCode> countryCodeList = CountryCode.findByName(kraj);
+        String kod_kraju = countryCodeList.get(0).toString();
 
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + miasto + "," +
+                kod_kraju + "&appid=" + API_KEY;
+        StringBuilder weather = new StringBuilder();
+
+        //Łączenie z pogodowym API
         try {
             URLConnection connection = new URL(url).openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line;
 
             while ((line = reader.readLine()) != null) {
-                weather += line;
+                weather.append(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        JSONObject json = new JSONObject(weather);
-        String description = json.getJSONArray("weather").getJSONObject(0).getString("description");
+        //Serializowanie JSONa
+        JSONObject json = new JSONObject(weather.toString());
+        String description = json.getJSONArray("weather").
+                getJSONObject(0).getString("description");
         double temperature = json.getJSONObject("main").getDouble("temp");
+        //Z kelvina na celcius
+        double celsius = temperature / 273;
+        celsius = round(celsius, 2);
 
-        return "Pogoda w " + city + ": " + description + ", temperatura: " + temperature + "°C";
+        return "Pogoda w " + miasto + ", " + kraj +
+                ": " + description + ", temperatura: " + celsius + "°C";
     }
 
     // Metoda pobierająca kurs wymiany waluty z serwisu NBP
-    private static double getRateFor(String kod_waluty) {
+    public double getRateFor(String kod_waluty) {
         String url = "http://api.nbp.pl/api/exchangerates/rates/a/" + kod_waluty + "/?format=json";
         String exchangeRate = "";
 
@@ -58,7 +80,7 @@ public class Service {
     }
 
     // Metoda pobierająca kurs złotego wobec danej waluty z serwisu NBP
-    private static double getNBPRate() {
+    public double getNBPRate() {
         String url = "http://api.nbp.pl/api/exchangerates/rates/a/" + "PLN" + "/?format=json";
         String exchangeRate = "";
 
